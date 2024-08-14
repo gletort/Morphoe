219626eac49164cef02d1290ff483651a7835aaa
Gaelle Letort <gaelle.letort.gl@gmail.com>
2024-08-14 15:19:03 +0200

    README more details

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # Morphoe
+[![License BSD-3](https://img.shields.io/pypi/l/morphoe.svg?color=green)](https://github.com/gletort/Morphoe/blob/main/LICENSE)
+
 Morphogenesis of olfactory epithelium.
 
 Python code to simulate migration of early olfactory neurons along the telencephalon.
 The matrix (telencephalon) is represented as an immobile and uncrossable horseshoe-like barrier (paraboloid).
 Cells are distributed along the telencephalon and follow a chemotaxis gradient (mimicking Cxcl12a signalling) to converge in two clusters.
 
+![Example of a simulation](https://github.com/gletort/Morphoe/raw/main/imgs/simu.png)
 
+# Installation/Usage
+
+There is no dependencies to install, you can use it directly on python (tested on python 3.8).
+
+To use it, call the `eon.py` main file in the simulation directory containing your parameter file `params.py`. Templates of parameter files are given in the folder [`simulation_template`](https://github.com/gletort/Morphoe/simulation_template) and in [our github repository associated to our publication](https://github.com/JulieBatut/Zilliox-Letort_2024/tree/main).
+
+To run it, you can go inside the simulation directory and type in a Terminal:
+```
+python path_to_Morphoe_src_folder/eon.py 
+```
 
 # References
 
 Refer to our publication Zilliox, Letort et al. for more details about the model.
 If you use this code or part of this code, please cite our publication or/and this repository.
+
+## License
+Distributed under the terms of the [BSD-3] license, "Morphoe" is free and open source software
---
+++ simulation_template/params.py
@@ -0,0 +1,62 @@
+
+### Parameters
+N = 60  ## nb cells, /6
+uniform = 1  ## uniform distribution of initial cells
+subN = [4,14,14]  ## if not uniform only
+dt = 0.0005 ## time step
+tmax = 6 # 7    ## simu time
+
+chemop = 2 #2 #2    ## ampltiude of chemo force
+chemoline = 0.02  ## amplitude of chemo for lines
+D = 0.001      ## amplitude of random motion
+v0 = 0.1     ## amplitude of polarized motion
+tau = 10     ## persistence coefficient (memory of past motion)
+
+d_interaction = 1  ## threshold distance to interact
+d_eq = 0.5           ## equilibrium dist between cells ~ diameter
+cell_cell = 0.7      ## amplitude of cell-cell interactions
+cell_mat = 4         ## amplitude of cell-matrix interaction
+out = 100            ## coefficient of rep if cell is inside matrix
+
+push = 0
+
+# options
+neurogenine_mutant = 0   ## neurogenine mutant: chemo 0 from 0, nstep/2
+d_cell_cell = 0          ## cell-cell interactions of different values
+d_cell_mat = 0           ## if cell-matrix interactions of diffrent values
+no_ecm_adhesion = 0      ## if 1: mutant, cells cannot adhtere to matrix
+all_matrix_chemo = 0     ## all matrix (encephale) is source of chemo-attract
+
+central_point_source = 1  ## positive if central point of matrix is source
+repulsion_sources = 0    ## add repulsive sources instead of chemo attractive source
+line_source = 0         ## vertical source 1, 2 horizontal source
+yline  = 0              ## position of horizontal line source
+ysource  = 0              ## position of horizontal line source
+chemo_cte = 0.5          ## chemotaxis force doesn't depend on distance to the source (cte gradient)
+
+# if repulsion sources, position of the 2 sources
+source1x = 0
+source1y = 3.8
+source2x = -1.25
+source2y = -3.8
+source3x = 1.25
+source3y = -3.8
+inhibition_zones = 0     ## if add inhibition zones or not
+inh_min = -1.5
+inh_max = 1.5
+inhib_coeff = 0.5
+
+shape = 'quadratic' ## encephale shape (quadratic~parabole, cylindric)
+yecm = 3.8          #3.8 quad, 3 cyl
+xecm = 1.0          #1
+antlim = 1.5        ## limit ot consider cell as anterior
+postlim = -1.5
+colant = (0.3,0.85,0.9,1) #"mediumturquoise"
+colmid = (0.25, 0.3,0.97,1) #"midnightblue"
+colpos = (0.1,0.1,0.5,1) 
+
+make_movie = 0     ## save movie
+mfreq = 200        ## frequency of saving image
+nrepet = 16         ## nb de repetition (n+1)
+dataFreq = 50      ## frequency of saving data point
+
